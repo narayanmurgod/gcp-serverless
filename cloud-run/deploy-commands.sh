7072aca9b953b5df495c7329e1f7ac7fc72d0f25
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-14 15:13:11 +0000

    uploading app files

---
+++ cloud-run/deploy-commands.sh
@@ -0,0 +1,22 @@
+# First Approach
+
+# Step-1 - Build the image 
+docker build -t basic-web-app .
+
+# Step-2 - Test the image locally 
+docker run -p 5000:5000 basic-web-app
+
+# Step-3 - Tag the image locally 
+docker tag basic-web-app asia-southeast1-docker.pkg.dev/cts05-murgod/serverless/basic-cloud-run-hello-app:latest
+
+# Step-4 - Push the image to Google Cloud Registry 
+docker push asia-southeast1-docker.pkg.dev/cts05-murgod/serverless/basic-cloud-run-hello-app
+
+# Deply to Cloud Run 
+gcloud run deploy basic-cloud-run-hello-app \
+  --source . \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --max-instances 4 \
+  --min-instances 2 
