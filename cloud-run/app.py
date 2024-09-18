580abf3177ff061c96671faff07e4d7f6a93bea2
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-18 06:31:21 +0000

    changed all files

--- cloud-run/app.py
+++ cloud-run/app.py
@@ -1,10 +1,11 @@
-from flask import Flask 
-app = Flask(__name__) 
+import os
+from flask import Flask
 
-@app.route('/') 
-def hello(): 
-	return "Hello Nani"
+app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "Hello Sid!!"
 
-if __name__ == "__main__": 
-	app.run(host ='0.0.0.0', port = 5001, debug = True) 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8081)
