3fb4402328990fbdadf5036547875ee547a2d307
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-18 07:07:35 +0000

    old example

--- cloud-run/app.py
+++ cloud-run/app.py
@@ -1,11 +1,14 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello Sid!!"
+@app.route('/')
+def hello():
+    return 'Hello, this is a Flask app running inside Docker!'
+@app.route('/greet')
+def greet():
+    return 'Hi.. Hope you are doing well'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
