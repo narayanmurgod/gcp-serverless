d80f305002cff8762868a92196c3de573d7dce8d
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-18 07:14:14 +0000

    commands updated

--- cloud-run/deploy-commands.sh
+++ cloud-run/deploy-commands.sh
@@ -4,7 +4,7 @@
 docker build -t basic-web-app .
 
 # Step-2 - Test the image locally 
-docker run -p 8081:8081 basic-web-app
+docker run -p 5000:5000 basic-web-app
 
 # Step-3 - Tag the image locally 
 docker tag basic-web-app asia-southeast1-docker.pkg.dev/cts05-murgod/serverless/basic-cloud-run-hello-app:latest
@@ -17,7 +17,7 @@ gcloud run deploy basic-cloud-run-hello-app \
   --source . \
   --platform managed \
   --region us-central1 \
-  --port 8081\
+  --port 5000\
   --allow-unauthenticated \
   --max-instances 4 \
   --min-instances 2 
